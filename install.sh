98dc3f6bbc69e3c99eb00671208c2094f31db4c8
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2026-05-06 04:03:36 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,42 @@
+chu() {
+if [ -e Ultroid ];then
+cd Ultroid
+msg Already cloned
+exit
+fi
+}
+fold() {
+cd ~
+if [ -e Termux-Ultroid ];then
+cd Termux-Ultroid
+else
+mkdir Termux-Ultroid
+cd Termux-Ultroid
+fi
+chu
+}
+
+msg starting cloning ultroid....
+fold
+git clone https://github.com/TeamUltroid/Ultroid > /dev/null 2>&1
+msg cloned [ √ ]
+fold
+msg installing ultroid requirement [ please wait.... ]
+if command -v pip3;then
+pip3 install -r requirements.txt  --no-cache-dir --force-reinstall > /dev/null 2>&1
+else
+pp i -r ~/T*d/*/req*  --no-cache-dir --force-reinstall > /dev/null 2>&1
+fi
+msg completed [ √ ]
+for c in pillow coloredlogs gitpython bs4 pytz enhancer telegraph aiohttp requests;do
+msg "installing $c"
+ch $c
+done
+fold
+msg -n "Type password: "
+read a
+unzip -P "$a" *zip
+tu cpp
+tu py
+tu st
+mv .env ~
